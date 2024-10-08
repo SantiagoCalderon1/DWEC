f92f998403b5a0bd9c71c70ea29f922282539614
Santiago Calderon <santiago.contactoyt@gmail.com>
2024-10-08 09:41:45 +0200

    EjercicioUd3act02-Ejercicio01 terminado

--- EjerciciosUd3Act02/ejercicio01.js
+++ EjerciciosUd3Act02/ejercicio01.js
@@ -1,119 +1,119 @@
-//Santiago Calderon Castaño
+// Santiago Calderón Castaño
 
-//Funciones para la ventana principal
+/**
+ * Me encontré con varios problemas al usar document.write.
+ * Algunas funciones no se ejecutaban y me lanzaba este erro:
+ * 
+ * Uncaught TypeError: Cannot read properties of null (reading 'value')
+ * at obtenerFecha (ejercicio01.js:48:54)
+ * 
+ * Me di cuenta que el document.write sobrescribía todo lo del DOM, por ende
+ * el input que me dabía dar la fecha desaparecia y en mi función obtener fecha
+ * se establecia a null su return.
+ * 
+ * lo solucioné con innerText, como solo me interesaba mostrar texto, hice 
+ * las etiquetas <p> y le inserté el texto a cada etiqueta correspondiente.
+ */
+
+
+// Funciones para la ventana principal
 function escribirPantallaPrincipal() {
-    let nombre = obtenerNombre().trim(); //uso trim para borrar posibles espacios en blanco del inicio o final
-    document.write("<h1>TAREA DWEC03</h1>");
-    document.write("<p>Buenos dias " + nombre + ".")
-    document.write("<p>Tu nombre tiene " + nombre.length + " caracteres, incluido espacios.")
+    let nombre = obtenerNombre().trim(); // Uso trim para borrar posibles espacios en blanco del inicio o final
+    document.getElementById("titulo").innerText = "TAREA DWEC03";
+    document.getElementById("texto2").innerText = "Buenos días " + nombre;
+    document.getElementById("texto3").innerText = "Tu nombre tiene " + nombre.length + " caracteres, incluidos espacios.";
 
-    nombre = nombre.toUpperCase();
-    // Uso toUpperCase para no tener que buscar tambien la a minuscula
+    nombre = nombre.toLowerCase(); // Uso toLowerCase para no tener que buscar también la 'a' minúscula
+    // Verificamos que la cadena incluya 'A'
     if (nombre.includes('A')) {
-        document.write("<p>La primera letra A de tu nombre está en la posicioón: " + nombre.indexOf('A') + "</p>")
-        document.write("<p>La última letra A de tu nombre está en la posicioón: " + nombre.lastIndexOf('A') + "</p>")
+        document.getElementById("texto4").innerText = "La primera letra A de tu nombre está en la posición: " + nombre.indexOf('A');
+        document.getElementById("texto5").innerText = "La primera letra A de tu nombre está en la posición: " + nombre.indexOf('A');
     } else {
-        document.write("<p>Tu nombre no contiene la letra A.</p>")
+        document.getElementById("texto4").innerText = "Tu nombre no contiene la letra A.";
     }
 
-    document.write("<p>Tu nombre menos las 3 primeras letras es: " + nombre.slice(3)+ "</p>");
+    //Aqui obtenemos una parte de la cadena desde el index 3 hasta el final, sin modificar la original.
+    document.getElementById("texto6").innerText = "Tu nombre menos las 3 primeras letras es: " + nombre.slice(3);
+    document.getElementById("texto7").innerText = "Tu nombre todo en mayúsculas es: " + nombre.toUpperCase();
 
-    document.write("<p>Tu nombre todo en mayusculas es mayúsculas es: " + nombre + "</p>"); //Nombre ya está en mayúsculas
+    document.getElementById("texto8").innerText = "Tu edad es: " + calcularEdad();
 
-    document.write("<p>Tu edad es: " + calcularEdad() + "</p>");
-
+    //Variables para el formato del texto9
     let fechaNacimiento = obtenerFecha();
-
     let diaSemana = fechaNacimiento.toLocaleString("es-ES", { weekday: "long" });
     let mes = fechaNacimiento.toLocaleString("es-ES", { month: "long" });
 
-    document.write("<p>Naciste un feliz " + diaSemana + ", " + fechaNacimiento.getDate() + " del año " + fechaNacimiento.getFullYear() + "</p>");
+    document.getElementById("texto9").innerText = "Naciste un feliz " + diaSemana + ", " + fechaNacimiento.getDate() + " de " + mes + " del año " + fechaNacimiento.getFullYear();
+    
+    document.getElementById("texto10").innerText = "El coseno de 180 es: " + calcularCoseno(180);
 
-    document.write("<p>El coseno de 180 es: " + calcularCoseno(180) + "</p>");
-
+    //Variable array que acontinuación usaremos
     let numeros = [34, 67, 23, 75, 35, 19];
-    //numeros.sort((a,b) => a - b).reverse();
-    document.write("<p>El número mayor de (" + numeros.join(",") + ") es: " + Math.max(...numeros) + "</p>");
+    // unimos el array usando join y separandolo con ',' , luego con MAth.max(...array) obtener el número más grande, solo funciona con números
+    document.getElementById("texto11").innerText = "El número mayor de (" + numeros.join(",") + ") es: " + Math.max(...numeros);
 
-    document.write("<p>Ejemplo de número al azar: " + Math.floor(Math.random() * 9000000000) + 1000000000) + "</p>";
+    // Generamos un número random de 10 digitos, porque hay 10 x en la actividad
+    document.getElementById("texto12").innerText = "Ejemplo de número al azar: " + (Math.floor(Math.random() * 9000000000) + 1000000000);
 }
 
+//Función para obtener el nombre, no tiene más.
 function obtenerNombre() {
-    return document.getElementById("input-nombre").value + " " + document.getElementById("input-apellido").value;
+    return document.getElementById("input-nombreCompleto").value;
 }
 
+//Función para obtener la fecha, devuelve un objeto fecha ya construido.
 function obtenerFecha() {
-    let dia = parseInt(document.getElementById("input-dia").value);
-    let mes = parseInt(document.getElementById("input-mes").value);
-    let año = parseInt(document.getElementById("input-año").value);
-    return new Date(año, (mes - 1), dia)
+    return new Date(document.getElementById("input-date").value);
 }
+
+/**Funcion para calcular la edad, tiene en cuenta si el dia de tu cumpleaños ya pasó.
+ * 
+ * Ejemplo: hoy 08/10/2024
+ *          Nacimiento 20/10/2003
+ * 
+ * 2024 -2003 = 21, pero yo aún no es 20 de octubre, así que en teoría no tengo 21 todavía,
+ * entonces también tiene en cuenta si es tu dia de cumpleaños no ha pasado, y si no ha pasado
+ * te resta un 1 año.
+ * 
+ * Lo hice con if tradicionales, pero así creo que también se entiende
+ * */
 
 function calcularEdad() {
     let fechaActual = new Date();
     let fechaNacimiento = obtenerFecha();
 
-    alert("Fecha actual:", fechaActual);
-    alert("Fecha de nacimiento:", fechaNacimiento);
-
-    
-    //Aqui uso un if ternario, me gusta más pero es más dificil de leer.
-    let edad = ((fechaActual.getFullYear() - fechaNacimiento.getFullYear() > 0) ?
+    // Aquí uso un operador ternario para calcular la edad
+    let edad = ((fechaActual.getFullYear() - fechaNacimiento.getFullYear())) > 0 ?
         (fechaActual.getMonth() > fechaNacimiento.getMonth() ||
-            (fechaActual.getMonth() === fechaNacimiento.getMonth() && fechaActual.getDate() >= fechaNacimiento.getDate()) ?
-            fechaActual.getFullYear() - fechaNacimiento.getFullYear() : fechaActual.getFullYear() - fechaNacimiento.getFullYear() - 1)
-        : 0); 
-
+            (fechaActual.getMonth() === fechaNacimiento.getMonth() && fechaActual.getDate() > fechaNacimiento.getDate())
+            ? fechaActual.getFullYear() - fechaNacimiento.getFullYear()
+            : fechaActual.getFullYear() - fechaNacimiento.getFullYear() - 1)
+        : 0;
     return edad;
-    /**
-     *  Primero se verifica si el año actual es mayor que el año de nacimiento.
-        Segundo se verifica si ya ha pasado el cumpleaños (comparando el mes y el día).
-        Tercero, se calcula la edad, teniendo en cuenta si ya has cumplido años o no.
-     */
-
-    /** Empleando con un if tradicional. Es más sencillo de leer pero más largo
-    if ((fechaActual.getFullYear() - fechaNacimiento.getFullYear()) > 0) {
-        if ((fechaActual.getMonth() > fechaNacimiento.getMonth()) || (fechaActual.getMonth() === fechaNacimiento.getMonth() && fechaActual.getDate() >= fechaNacimiento.getDate())) {
-            edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
-        }else{
-            edad = (fechaActual.getFullYear() - fechaNacimiento.getFullYear()) - 1 ;
-        }
-    }
-     */
 }
 
+//Función para calcular el coseno de un número, me apoyé de internet, no sé como va esto de las formulas.
 function calcularCoseno(grados) {
-    /**
-     * Versión larga.
-    let radianes = grados * (Math.PI / 180);
-    return Math.cos(radianes);
-    
-    La función Math.cos() devuelve el coseno de un número en radianes.
-     */
-
-    //Versión corta
-    return Math.cos(grados * (Math.PI / 180));
-
+    return Math.cos(grados * (Math.PI / 180)); // Convierte grados a radianes y calcula el coseno
 }
 
-
-
-//Funciones para la ventana secundaria
+/** Funcione para la ventana secundaria
+ * En mi ordenador no me deja que la ventana se abra en pop-up, pero en el instituto sí.
+ * 
+ * Lo de que no se deje redimensionar no lo he logrado, creía que pasadole por parametros
+ * una medida definida no dejaría redimensionarla
+ *  */ 
 function abrirVentana() {
     let ventanaSecundaria = window.open("", "Acceso", "width=400,height=400");
     ventanaSecundaria.document.write(datosNuevaVentana(ventanaSecundaria.document));
-    //Enviamos document correspondiente a la pagina secundaria por parametros.
+    // Enviamos document correspondiente a la página secundaria por parámetros. porque si no tomaria el document de la página principal
 }
 
 function datosNuevaVentana(doc) {
-    /** 
-     * doc corresponde a la ventana actual, que es la secundaria, no la principal
-     * si usara document. correspondria a la página principal
-     * */
+    // Contenido de la nueva ventana, no me gusta mucho de esta forma, pero es más sencillo que construirla totalmente de 0
     let contenido =
         `<!DOCTYPE html>
         <html lang="es">
-
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -122,34 +122,31 @@ function datosNuevaVentana(doc) {
         <body>
             <h3>Ejemplo de Ventana Nueva</h3>
             <div id="contenedor">
-                <!--Suelo usar span cuando debo insertar datos dentro de una etiqueta ya creada-->
                 <p>URL Completa: <span id="url-completa">${doc.URL}</span></p>
                 <p>Protocolo utilizado: <span id="protocolo">${doc.location.protocol}</span></p>
                 <p>Nombre del código del navegador: <span id="codigo-navegador">${obtenerNavegador()}</span></p>
-                <p>Java <span id="java-disponible">${(navigator.javaEnabled() ? "SI" : "NO")}</span> disponible en esta ventana.</p>
-                <div id="contenedor-ifame">
-                    <!-- Como google rechazaba la conexión, udé wikipedia, que se que seguro funciona. -->
+                <p>Java <span id="java-disponible">${navigator.javaEnabled() ? "SI" : "NO"}</span> disponible en esta ventana.</p>
+                <div id="contenedor-iframe">
                     <iframe src="https://es.wikipedia.org/wiki/Wikipedia:Portada" width="800" height="600"></iframe>
                 </div>
             </div>
         </body>
-        </html>
-        `;
+        </html>`;
     return contenido;
-
 }
 
-// como navigator.userAgent; el formato lo da horrible, pues pensé que una funcion para darle formato esta bien
+// Función para obtener el navegador, esto simplemente es para darle formato, ya que da un código muy largo y casi inentendible
 function obtenerNavegador() {
     let userAgent = navigator.userAgent;
+
     if (userAgent.includes("Chrome") && !userAgent.includes("Edge")) {
         return "Google Chrome";
     } else if (userAgent.includes("Firefox")) {
         return "Mozilla Firefox";
     } else if (userAgent.includes("MSI") || userAgent.includes("Trident")) {
-        return "Internet Explorer"
+        return "Internet Explorer";
     } else if (userAgent.includes("Edg")) {
-        return "Microsoft Edge"
+        return "Microsoft Edge";
     } else if (userAgent.includes("OPR")) {
         return "Opera";
     } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
@@ -159,5 +156,7 @@ function obtenerNavegador() {
     }
 }
 
-//window.onload = escribirPantallaPrincipal();
-
+//Pruebas que estuve haciendo, pero no me hizo falta.
+//window.onload = function () {
+//    escribirPantallaPrincipal()
+//};
