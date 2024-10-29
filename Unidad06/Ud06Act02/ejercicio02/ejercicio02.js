86573300567fee6c01cfb9d270935d8526e811c3
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2024-10-29 19:31:53 +0100

    terminar

---
+++ Unidad06/Ud06Act02/ejercicio02/ejercicio02.js
@@ -0,0 +1,32 @@
+const imgs = [
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg",
+  "img4.jpg",
+  "img5.jpg",
+  "img6.jpg",
+  "img7.jpg",
+  "img8.jpg",
+  "img9.jpg",
+  "img10.jpg",
+  "img11.jpg",
+  "img12.jpg",
+  "img13.jpg",
+  "img14.jpg",
+  "img15.jpg",
+  "img16.jpg",
+  "img17.jpg",
+  "img18.jpg",
+  "img19.jpg",
+  "img20.jpg",
+];
+
+/*<div id="container">
+        <img src="" alt="">
+        <div id="flechaIzquierda"></div>
+        <div id="flechaDerecha"></div>
+    </div> */
+
+function cambiarImagen() {
+    
+}
