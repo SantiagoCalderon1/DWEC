7d997f23545164e9c6baa77f6ae96aa6dacfbcee
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2025-01-27 19:23:10 +0100

    ejercicio m terminado

--- Unidad11/ejemploM/src/app/app-routing.module.ts
+++ Unidad11/ejemploM/src/app/app-routing.module.ts
@@ -4,69 +4,39 @@ import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 import { ListaComponent } from './empleados/components/lista/lista.component';
 import { ListaComponent as ListaFacturas } from './facturas/componets/lista/lista.component';
 import { ListaComponent as ListaNominas } from './nominas/componets/lista/lista.component';
+import { ListaComponent as ListaComentarios } from './comments/components/lista/lista.component';
+import { CommentComponent } from './comments/components/comment/comment.component';
 import { EmpleadoComponent } from './empleados/components/empleado/empleado.component';
 import { FacturaComponent } from './facturas/componets/factura/factura.component';
 import { NominaComponent } from './nominas/componets/nomina/nomina.component';
 import { empleadoGuard } from './empleados/empleado.guard';
 import { abandonarPaginaGuard } from './empleados/abandonar-pagina.guard';
-// import { empleadoResolveGuard } from './empleados/empleado-resolve.guard';
 import { LoginComponent } from './login/components/login/login.component';
 import { loginGuard } from './login/login.guard';
 import { LogoutComponent } from './login/components/logout/logout.component';
 
-import { ListaComponent as ListaComments } from './comments/components/lista/lista.component';
-import { CommentsComponent } from './comments/components/comments/comments.component';
-
-
-
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
-  {
-    path: 'bienvenido',
-    component: BienvenidoComponent,
-    canActivate: [loginGuard],
-  },
+  { path: 'bienvenido', component: BienvenidoComponent, canActivate: [loginGuard] },
   { path: 'empleados', component: ListaComponent, canActivate: [loginGuard] },
-  {
-    path: 'empleados/:tipo/:id',
-    component: EmpleadoComponent,
-    canActivate: [loginGuard, empleadoGuard],
-    canDeactivate: [abandonarPaginaGuard],
-  },
+  { path: 'empleados/:tipo/:id', component: EmpleadoComponent, canActivate: [loginGuard, empleadoGuard],
+    canDeactivate: [abandonarPaginaGuard], },
   { path: 'facturas', component: ListaFacturas, canActivate: [loginGuard] },
-  {
-    path: 'facturas/:tipo/:id',
-    component: FacturaComponent,
-    canActivate: [loginGuard],
-    canDeactivate: [abandonarPaginaGuard],
-  },
+  { path: 'facturas/:tipo/:id', component: FacturaComponent, canActivate: [loginGuard], canDeactivate: [abandonarPaginaGuard] },
   { path: 'nominas', component: ListaNominas, canActivate: [loginGuard] },
-  {
-    path: 'nominas/:tipo/:id',
-    component: NominaComponent,
-    canActivate: [loginGuard],
-    canDeactivate: [abandonarPaginaGuard],
-  },
-
-
-
-  { path: 'comments', component: ListaComments, canActivate: [loginGuard] },
-  {
-    path: 'comments/:tipo/:id',
-    component: CommentsComponent,
-    canActivate: [loginGuard],
-    canDeactivate: [abandonarPaginaGuard],
-  },
-  
+  { path: 'nominas/:tipo/:id', component: NominaComponent, canActivate: [loginGuard], canDeactivate: [abandonarPaginaGuard] },
+  { path: 'comentarios', component: ListaComentarios, canActivate: [loginGuard] },
+  { path: 'comentarios/:tipo/:id', component: CommentComponent, canActivate: [loginGuard], canDeactivate: [abandonarPaginaGuard] },
   // Ruta por defecto (vacía) -> Redirigir a /welcome
   { path: '', redirectTo: '/bienvenido', pathMatch: 'full' },
   // Ruta que no coincide con ninguna de las anteriores
-  { path: '**', redirectTo: '/bienvenido', pathMatch: 'full' },
+  { path: '**', redirectTo: '/bienvenido', pathMatch: 'full' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
